0a5aceb942de68d69fc6d70638842c2740dc817d
Álvaro Nicolás Ojeda <135424514+ojalv@users.noreply.github.com>
2025-06-22 16:48:51 -0300

    Agregar rutas

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import express from "express";
 import { sequelize } from "./src/models/index.js"; // Importa la instancia (nota el .js)
 import { syncDB, authDB } from "./src/utils/db.js";
 import categoriaRoutes from "./src/routes/categoriaRoutes.js";
+import imagenRoutes from "./src/routes/imagenRoutes.js";
+import productoRoutes from "./src/routes/productoRoutes.js";
+import ordenDeCompraRoutes from "./src/routes/ordenDeCompraRoutes.js";
+import itemOrdenDeCompraRoutes from "./src/routes/itemOrdenDeCompraRoutes.js";
+import clienteRoutes from "./src/routes/clienteRoutes.js";
+import empleadoRoutes from "./src/routes/empleadoRoutes.js";
 import marcaRoutes from "./src/routes/marcaRoutes.js";
 
 const app = express();
@@ -13,7 +19,13 @@ app.use(express.json());
 
 // Rutas de API
 app.use('/categorias', categoriaRoutes);
-app.use('/marcas', marcaRoutes); 
+app.use('/marcas', marcaRoutes);
+app.use('/imagenes', imagenRoutes);
+app.use('/productos', productoRoutes);
+app.use('/ordenes', ordenDeCompraRoutes);
+app.use('/items', itemOrdenDeCompraRoutes);
+app.use('/clientes', clienteRoutes);
+app.use('/empleados', empleadoRoutes);
 
 // Ruta de prueba
 app.get("/", (req, res) => {
